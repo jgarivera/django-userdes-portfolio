f3071caa25d3bebbd52df75a122170b9dd8f1225
JG Rivera <rivera.jga10@gmail.com>
2020-10-18 20:42:36 +0800

    Heroku tweak

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -126,4 +126,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Heroku settings configuration
 django_heroku.settings(locals())
